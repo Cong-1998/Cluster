54e3248e1cf93e10a9e5590c353925fdbfb0224f
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-10-12 17:31:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import re
 import pandas as pd
 import numpy as np
 import nltk
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from wordcloud import WordCloud 
 import matplotlib.pyplot as plt
