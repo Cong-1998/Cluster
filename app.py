e040b92a85cfde2e94bf5d2b1b4e2c6f467f6db1
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-10-12 16:46:17 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+# import libraries
+import streamlit as st
+import malaya
+import re
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize
+from wordcloud import WordCloud 
+import matplotlib.pyplot as plt
+from gsdmm import MovieGroupProcess
+from topic_modelling import processing
+
+def run():
+    file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+    if file_upload is not None:
+        data = pd.read_csv(file_upload)
+        st.write(data)
+        wc1, wc2 = processing(data)
+        st.image(wc1.to_image())
+        st.image(wc2.to_image())
+
+if __name__ == '__main__':
+    run()
