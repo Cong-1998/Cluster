9fd241790fd2d20e8dbe1086f993bce03b5f46ca
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-10-12 18:07:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def run():
     if file_upload is not None:
         data = pd.read_csv(file_upload, encoding='unicode_escape')
         st.write(data)
-        wc1, wc2 = processing(data, gensim, malaya, word_tokenize, WordCloud)
+        wc1, wc2 = processing(data, gensim, malaya, word_tokenize, WordCloud, np)
         st.image(wc1.to_image())
         st.image(wc2.to_image())
 
