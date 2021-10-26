7d873ed24f94c10b41132c496b9aef2ddb03c8d8
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-10-26 17:39:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 nltk.download('stopwords')
+nltk.download('punkt')
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import base64
