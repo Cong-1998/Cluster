b2b27f063feaa49ef75f9850b1f24ca3ea737796
Cong-1998 <92134755+Cong-1998@users.noreply.github.com>
2021-10-26 17:49:12 +0800

    Update topic_modelling.py

--- topic_modelling.py
+++ topic_modelling.py
@@ -1,147 +1,122 @@
-# import libraries
-import streamlit as st
-import malaya
-import re
-import pandas as pd
-import numpy as np
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-nltk.download('punkt')
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-import base64
-import gensim
-from gensim import corpora, models
-from streamlit_tags import st_tags, st_tags_sidebar
-from streamlit_player import st_player
-from gsdmm import MovieGroupProcess
-from topic_modelling import processing
-from topic_modelling import token
-from topic_modelling import topic_model
-from topic_modelling import top_words
-from topic_modelling import create_topics_dataframe
-from topic_modelling import create_WordCloud
 
-class Toc:
-    def __init__(self):
-        self._items = []
-        self._placeholder = None
-    
-    def title(self, text):
-        self._markdown(text, "h1")
+def token(senten, word_tokenize):
+    results = []
+    for sentence in senten:
+        results.append(word_tokenize(sentence))
+    return results
 
-    def header(self, text):
-        self._markdown(text, "h2", " " * 2)
+def topic_model(reviews_lemmatized, gensim, np, MovieGroupProcess, int_val):
+    np.random.seed(0)
 
-    def subheader(self, text):
-        self._markdown(text, "h3", " " * 4)
+    # initialize GSDMM
+    gsdmm = MovieGroupProcess(K=int_val, alpha=0.1, beta=0.3, n_iters=int_val)
 
-    def placeholder(self, sidebar=True):
-        self._placeholder = st.sidebar.empty() if sidebar else st.empty()
+    # create dictionary of all words in all documents
+    dictionary = gensim.corpora.Dictionary(reviews_lemmatized)
 
-    def generate(self):
-        if self._placeholder:
-            self._placeholder.markdown("\n".join(self._items), unsafe_allow_html=True)
-    
-    def _markdown(self, text, level, space=""):
-        key = "-".join(text.split()).lower()
-        st.markdown(f"<{level} id='{key}'>{text}</{level}>", unsafe_allow_html=True)
-        self._items.append(f"{space}* <a href='#{key}'>{text}</a>")
+    # filter extreme cases out of dictionary
+    dictionary.filter_extremes(no_below=int_val, no_above=0.5, keep_n=100000)
 
-# hide menu bar
-st.markdown(""" <style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-</style> """, unsafe_allow_html=True)
-    
-# set up layout
-padding = 1
-st.markdown(f""" <style>
-    .reportview-container .main .block-container{{
-        padding-top: {padding}rem;
-        padding-right: {padding}rem;
-        padding-left: {padding}rem;
-        padding-bottom: {padding}rem;
-    }} </style> """, unsafe_allow_html=True)
+    # create variable containing length of dictionary/vocab
+    n_terms = len(dictionary)
 
-# set up title
-st.title("GSDMM Topic Modeling")
-st.write('\n')
+    # fit GSDMM model
+    model = gsdmm.fit(reviews_lemmatized, n_terms)
+    doc_count = np.array(gsdmm.cluster_doc_count)
 
-# set up sidebar
-st.sidebar.header("Table of Content")
-toc = Toc()
-toc.placeholder()
+    # topics sorted by the number of document they are allocated to
+    top_index = doc_count.argsort()[-int_val:][::-1]
 
-# upload file
-toc.header("Upload csv file")
-file_upload = st.file_uploader("", type=["csv"])
-if file_upload is not None:
-    data = pd.read_csv(file_upload, encoding='unicode_escape')
-    st.write(data)
-    name = file_upload.name.replace('.csv', '')
-    name = name+"_labelled.csv"
-
-# select cluster
-toc.header("Select the number of clusters")
-int_val = st.number_input('', min_value=1, max_value=30, value=5, step=1)
-
-# add stopword
-list_stop = []
-my_expander = st.expander(label='Advanced Setting')
-with my_expander:
-    st.write("This tab is to remove stopwords.")
-    st.subheader("[What is stopwords](#stopwords) :question:")
-    keywords = st_tags(
-        label='Enter Stopwords: :point_down:',
-        text='Press enter to add more',
-        value=[],
-        key="aljnf")
-    st.write(keywords)
-    st.write("*Don't collapse this tab.")
-list_stop = keywords
-
-# run the program
-result = st.button("Run")
-if result:
-    wc = []
+    # show the top 20 words in term frequency for each cluster
     ans = []
-    st.write("Be patient, need to wait 1 to 2 minutes :smile:")
-    st.write("If raising error, please reduce the number of clusters")
-    wc, ans, final_df = processing(data, gensim, malaya, word_tokenize, np, MovieGroupProcess, pd, WordCloud, int_val, stopwords, list_stop)
+    ans = top_words(gsdmm, gsdmm.cluster_word_distribution, top_index, 15)
+    return top_index, gsdmm, ans
 
-    # download labelled file
-    st.write("Below is the labelled file, click it tp download.")
-    csv = final_df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings
-    linko= f'<a href="data:file/csv;base64,{b64}" download='+name+'>Download csv file</a>'
-    st.markdown(linko, unsafe_allow_html=True)
-    st.write(ans)
-    for i in range(len(wc)):
-        st.markdown('Most used words in type '+str(i+1))
-        st.image(wc[i].to_image())
-        st.write('\n')
-st.write('\n')
+def top_words(gsdmm, cluster_word_distribution, top_cluster, values):
+    ans = []
+    for cluster in top_cluster:
+        sort_dicts =sorted(gsdmm.cluster_word_distribution[cluster].items(), key=lambda k: k[1], reverse=True)[:values]
+        ans.append("\nCluster %s : %s"%(cluster,sort_dicts))
+    return ans
 
-# how to use
-toc.header("How to Use")
-st.write("1. Please upload csv file and make sure your data are in first column.")
-st.write("*How to convert excel file to [csv file](#csv-file).")
-st.write("2. Option: You can expand Advanced Setting tab to add new [stopword](#stopwords).")
-st.write("3. Please select the number of clusters.")
-st.write("4. Please click the 'Run' button.")
-st.write('\n')
+def create_topics_dataframe(pd, data_text,  mgp, threshold, topic_dict, lemma_text):
+    result = pd.DataFrame(columns=['Text', 'Topic', 'Lemma-text'])
+    for i, text in enumerate(data_text):
+        result.at[i, 'Text'] = text
+        result.at[i, 'Lemma-text'] = lemma_text[i]
+        prob = mgp.choose_best_label(lemma_text[i])
+        if prob[1] >= threshold:
+            result.at[i, 'Topic'] = topic_dict[prob[0]]
+        else:
+            result.at[i, 'Topic'] = 'Other'
+    return result
 
-st.subheader("CSV file")
-st.write('This video will teach you how to convert excel file to csv file.')
-# Embed a youtube video
-st_player("https://www.youtube.com/watch?v=IBbJzzj5r90")
-st.write('\n')
+def processing(data, gensim, malaya, word_tokenize, np, MovieGroupProcess, pd, WordCloud, int_val, stopwords, list_stop):
+    df = data.iloc[:, 0]
 
-st.subheader("Stopwords")
-st.write("Stopwords are the words which does not add much meaning to a sentence. They can safely be ignored without sacrificing the meaning of the sentence. For example, the words like ada, apa, dia, a, an, the etc.")
-st.write("*Here are [stopwords](https://github.com/Cong-1998/Cluster/blob/main/stopwords.txt). :point_left:")
+    # remove characters and turn to lower case
+    df1 = df.str.lower().str.replace('[^\w\s]','')
 
-toc.generate()
+    # change text abbreviations to original word
+    df1 = df1.str.replace(r'\bx\b', 'tidak')
+    df1 = df1.str.replace(r'\btak\b', 'tidak')
+    df1 = df1.str.replace(r'\borg\b', 'orang')
+    df1 = df1.str.replace(r'\bdgn\b', 'dengan')
+    df1 = df1.str.replace(r'\bmora\b', 'moratorium')
+    df1 = df1.str.replace(r'\bni\b', 'ini')
+    df1 = df1.str.replace(r'\btu\b', 'itu')
+
+    # remove unwanted word
+    df1 = df1.str.replace('\n', '')
+    df1 = df1.str.replace(r'\bla\b', '')
+    df1 = df1.str.replace(r'\bje\b', '') 
+
+    # remove stopword
+    stop_words = malaya.text.function.STOPWORDS
+    stops = set(stopwords.words('english'))
+    stop_words.update(stops)
+    stop_words.update(list_stop)
+    df2 = df1.apply(lambda x: ' '.join([word for word in x.split() if word not in (stop_words)]))
+
+    # dataframe change to list
+    list_dat = df2.values.tolist()
+    
+    # tokenize word
+    reviews_lemmatized = token(list_dat, word_tokenize)
+
+    # GSDMM for the topic modeling
+    ans = []
+    top_index, gsdmm, ans = topic_model(reviews_lemmatized, gensim, np, MovieGroupProcess, int_val)
+
+    # give name to the cluster
+    list_topic = list(top_index)
+    topic_dict = {}
+    topic_names = []
+    for i in range(len(list_topic)):
+        topic_names.append("Cluster " + str(list_topic[i]))
+
+    for i, topic_num in enumerate(top_index):
+        topic_dict[topic_num]=topic_names[i]
+
+    # create dataframe with topic
+    result = create_topics_dataframe(pd, data_text=df1, mgp=gsdmm, threshold=0.3, topic_dict=topic_dict, lemma_text=reviews_lemmatized)
+    result['Lemma_text'] = result['Lemma-text'].apply(lambda row: ' '.join(row))
+    result = result.drop('Lemma-text', axis=1)
+
+    # create dataframe with label
+    final_df = pd.concat([df, result['Topic']], axis=1)
+
+    # create word clouds
+    wc = []
+    for i in range(int_val):
+        wc.append(create_WordCloud(WordCloud, result['Lemma_text'].loc[result.Topic == topic_names[i]], title=("Most used words in "+topic_names[i])))
+    return wc, ans, final_df
+
+def create_WordCloud(WordCloud, data, title=None):
+    wordcloud = WordCloud(width = 400, height = 400,
+                          collocations = False,
+                          background_color ='white',
+                          min_font_size = 14
+                          ).generate(" ".join(data.values))
+
+    return wordcloud
